07a30cd7bf055f69853626bb5f4b857181920cf9
Theo Bittencourt <theodrums@gmail.com>
2015-07-04 22:16:34 -0300

    Move directives from app.js to directives.js

--- www/js/app.js
+++ www/js/app.js
@@ -205,62 +205,6 @@ gotMusicApp.run(function($rootScope) {
     };
 });
 
-gotMusicApp.directive('navigationTabs', function() {
-  return {
-    templateUrl: 'directives/navigation-tabs/navigation-tabs.html',
-    controller: 'NavigationCtrl'
-  };
-});
-
-gotMusicApp.directive('titleBar', function() {
-  return {
-    templateUrl: 'directives/title-bar/title-bar-new.html',
-    //controller: 'NavigationCtrl'
-  };
-});
-
-gotMusicApp.directive('swiper', ['$timeout', function($timeout) {
-  return {
-    restrict: 'A',
-    link: function(scope, element, attrs) {
-      element.css({width: window.innerWidth - ITEM_MARGIN});
-
-      function init() {
-        var params = {
-          spaceBetween: ITEM_MARGIN / 2
-        };
-
-        function adjustWrapperHeight() {
-          var nextSlide = $(this.slides[this.activeIndex]);
-          var newHeight = $('.content', nextSlide).outerHeight() +
-                          $('.title-wrapper', nextSlide).outerHeight();
-
-          $(this.wrapper[0]).height(newHeight);
-        }
-
-        params.onInit = function (swiper) {
-          adjustWrapperHeight.apply(swiper);
-        };
-
-        params.onSlideChangeStart = function (swiper) {
-          adjustWrapperHeight.apply(swiper);
-        };
-
-        $timeout(function(){
-          if (attrs.onSlideChangeEnd) {
-            params.onSlideChangeEnd = scope[attrs.onSlideChangeEnd];
-          }
-
-          new Swiper(element, params);
-        });
-      }
-
-      if (attrs.swiperAutoinit) { init(); }
-      if (attrs.swiperInitOn) { scope.$on(attrs.swiperInitOn, init); }
-    }
-  };
-}]);
-
 gotMusicApp.service("videoService", function() {
   this.video_id = '';
   this.video_url = '';
---
+++ www/js/directives.js
@@ -0,0 +1,55 @@
+angular.module('gotMusicApp')
+  .directive('navigationTabs', function() {
+    return {
+      templateUrl: 'directives/navigation-tabs/navigation-tabs.html',
+      controller: 'NavigationCtrl'
+    };
+  })
+
+  .directive('titleBar', function() {
+    return {
+      templateUrl: 'directives/title-bar/title-bar-new.html',
+    };
+  })
+
+  .directive('swiper', ['$timeout', function($timeout) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        element.css({width: window.innerWidth - ITEM_MARGIN});
+
+        function init() {
+          var params = {
+            spaceBetween: ITEM_MARGIN / 2
+          };
+
+          function adjustWrapperHeight() {
+            var nextSlide = $(this.slides[this.activeIndex]);
+            var newHeight = $('.content', nextSlide).outerHeight() +
+                            $('.title-wrapper', nextSlide).outerHeight();
+
+            $(this.wrapper[0]).height(newHeight);
+          }
+
+          params.onInit = function (swiper) {
+            adjustWrapperHeight.apply(swiper);
+          };
+
+          params.onSlideChangeStart = function (swiper) {
+            adjustWrapperHeight.apply(swiper);
+          };
+
+          $timeout(function(){
+            if (attrs.onSlideChangeEnd) {
+              params.onSlideChangeEnd = scope[attrs.onSlideChangeEnd];
+            }
+
+            new Swiper(element, params);
+          });
+        }
+
+        if (attrs.swiperAutoinit) { init(); }
+        if (attrs.swiperInitOn) { scope.$on(attrs.swiperInitOn, init); }
+      }
+    };
+  }]);
