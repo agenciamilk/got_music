4a5fa3adb629d58e5876bcbf16025a4ee6a609bd
aluis92 <andre_luis9214@hotmail.com>
2015-11-03 18:08:36 -0200

    form abraham mateo atualizado

--- _abrahamMateo/js/main.js
+++ _abrahamMateo/js/main.js
@@ -1,22 +1,56 @@
+function al(args){
+  return document.querySelector(args);
+}
+
+function hide(args){
+  args.style.display = "none";
+}
+
+function show(args){
+  args.style.display = "block";
+}
+
 function getName(filename){
   var arr = filename.split(/\\/g);
   return arr.pop();
 };
 
-document.querySelector('.photo-upload').addEventListener('change',function(){
-  var filename = document.querySelector('.photo-upload').value;
+al('.photo-upload').addEventListener('change',function(){
+  var filename = al('.photo-upload').value;
   if(filename){
-    document.querySelector('.photo-name').innerHTML = getName(filename);
+    hide(al('.label-video'));
+    al('.photo-name').innerHTML = getName(filename);
   }else{
-    document.querySelector('.photo-name').innerHTML = getName("jpg e png");
+    show(al('.label-video'));
+    al('.photo-name').innerHTML = getName("jpg e png");
   }
 });
 
-document.querySelector('.video-upload').addEventListener('change',function(){
-  var filename = document.querySelector('.video-upload').value;
+al('.video-upload').addEventListener('change',function(){
+  var filename = al('.video-upload').value;
   if(filename){
-    document.querySelector('.video-name').innerHTML = getName(filename);
+    hide(al('.label-photo'));
+    al('.video-name').innerHTML = getName(filename);
   }else{
-    document.querySelector('.video-name').innerHTML = getName("mp4 e ogg");
+    show(al('.label-photo'));
+    al('.video-name').innerHTML = getName("mp4 e ogg");
   }
 });
+
+var allRadios = document.getElementsByName('q6_linkInterno');
+var booRadio;
+var x = 0;
+for(x = 0; x < allRadios.length; x++){
+
+    allRadios[x].onclick = function() {
+        if(booRadio == this){
+            this.checked = false;
+            booRadio = null;
+            show(al('.call2action'));
+        }else{
+            booRadio = this;
+            hide(al('.call2action'));
+            al('.call2action input').value = "";
+        }
+    };
+}
